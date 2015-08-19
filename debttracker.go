9bf4ec0dcfa8424d9165b245de77ddb69f1b1f95
Ben Doan <ben@bendoan.me>
2015-08-19 17:15:06 -0500

    added debt storage

--- debttracker.go
+++ debttracker.go
@@ -5,9 +5,19 @@ import (
 	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
-var templates = template.Must(template.ParseFiles("templates/index.html"))
+var (
+	templates = template.Must(template.ParseFiles("templates/index.html"))
+	debtStore []DebtItem
+)
+
+type DebtItem struct {
+	Person string
+	Amount Money
+	Note   string
+}
 
 type Money struct {
 	Cents int64
@@ -23,19 +33,36 @@ func (v Money) String() string {
 type DebtData struct {
 	Ower       string
 	OwedAmount Money
+	DebtStore  []DebtItem
 }
 
 func BaseHandler(w http.ResponseWriter, r *http.Request) {
-	data := DebtData{"Ben", Money{1450}}
+	data := DebtData{"Ben", Money{14}, debtStore}
 	err := templates.ExecuteTemplate(w, "index.html", data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+func HandleAddDebt(w http.ResponseWriter, r *http.Request) {
+	person := r.FormValue("person")
+	amount, _ := strconv.ParseInt(r.FormValue("amount"), 10, 64)
+	moneyAmount := Money{amount}
+
+	debtStore = append(debtStore, DebtItem{Person: person, Amount: moneyAmount, Note: ""})
+
+	http.Redirect(w, r, "/", 301)
+}
+
+func init() {
+	debtStore = make([]DebtItem, 0)
+}
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", BaseHandler)
+	r.HandleFunc("/", BaseHandler).Methods("GET")
+
+	r.HandleFunc("/", HandleAddDebt).Methods("POST")
 	//r.HandleFunc("/user/get", HandleUserGet)
 
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
@@ -44,5 +71,8 @@ func main() {
 	http.Handle("/", r)
 
 	fmt.Println("Listening on :8080")
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %v", err)
+	}
 }
